010893682cd8e37099c78445ff483f476aea7481
Chandong <kcd9511@gmail.com>
2023-12-28 11:27:32 +0900

    css추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 // src/App.tsx
 import React from 'react';
 import Weather from './components/Weather';
+import './styles/main.css';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className='container'>
       <Weather />
     </div>
   );
