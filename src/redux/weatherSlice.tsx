489213ab9054055f854887cc89d8672bcbb70164
Chandong <kcd9511@gmail.com>
2024-01-20 14:37:34 +0900

    예보수정

--- src/redux/weatherSlice.tsx
+++ src/redux/weatherSlice.tsx
@@ -1,3 +1,4 @@
+// weatherSlice.tsx
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { WeatherData, ForecastData } from "../types";
@@ -16,22 +17,28 @@ const initialState: WeatherState = {
 
 export const fetchWeatherData = createAsyncThunk(
   "weather/fetchWeatherData",
-  async (city: string) => {
-    const response = await axios.get<WeatherData>(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1b23dd586b036b96e6edcfe17c0c6e8f&units=metric`
-    );
-    const forecastResponse = await axios.get<{ list: ForecastData[] }>(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=1b23dd586b036b96e6edcfe17c0c6e8f&units=metric`
-    );
-    setForecastData(forecastResponse.data.list);
-    console.log(response.data)
-    console.log(forecastResponse.data.list)
-    
-    return response.data;
-    
+  async (city: string, { dispatch }) => {
+    try {
+      const response = await axios.get<WeatherData>(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1b23dd586b036b96e6edcfe17c0c6e8f&units=metric`
+      );
+      const forecastResponse = await axios.get<{ list: ForecastData[] }>(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=1b23dd586b036b96e6edcfe17c0c6e8f&units=metric`
+      );
+
+      // setForecastData 대신에 액션을 dispatch 하도록 변경
+      dispatch(setForecastData(forecastResponse.data.list));
+
+      console.log(response.data);
+      console.log(forecastResponse.data.list);
+
+      return response.data;
+    } catch (error) {
+      console.error("날씨 데이터를 가져오는 중 에러 발생:", error);
+      throw error;
+    }
   }
 );
-
 
 const weatherSlice = createSlice({
   name: "weather",
@@ -41,6 +48,7 @@ const weatherSlice = createSlice({
       state.city = action.payload;
     },
     setForecastData: (state, action) => {
+      // 직접 state.forecastData를 업데이트하도록 변경
       state.forecastData = action.payload;
     },
   },
