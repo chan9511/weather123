41149c95d9ee083582ceb6080d20d4b54213369a
Chandong <kcd9511@gmail.com>
2024-01-25 20:06:53 +0900

    날씨아이콘추가

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,20 +1,24 @@
 // Weather.tsx
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setCity, fetchWeatherData } from '../redux/weatherSlice';
-import { ForecastData } from '../types';
-import { RootState } from '../store/store';
-import '../styles/main.css';
-import { ThunkAction } from '@reduxjs/toolkit';
-import { ThunkDispatch } from 'redux-thunk';
-import { AnyAction } from 'redux';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setCity, fetchWeatherData } from "../redux/weatherSlice";
+import { ForecastData } from "../types";
+import { RootState } from "../store/store";
+import "../styles/main.css";
+import { ThunkAction } from "@reduxjs/toolkit";
+import { ThunkDispatch } from "redux-thunk";
+import { AnyAction } from "redux";
 
 const Weather: React.FC = () => {
   const dispatch = useDispatch<ThunkDispatch<RootState, any, AnyAction>>();
 
   const city = useSelector((state: RootState) => state.weather.city);
-  const weatherData = useSelector((state: RootState) => state.weather.weatherData);
-  const forecastData = useSelector((state: RootState) => state.weather.forecastData);
+  const weatherData = useSelector(
+    (state: RootState) => state.weather.weatherData
+  );
+  const forecastData = useSelector(
+    (state: RootState) => state.weather.forecastData
+  );
 
   const getWeather: ThunkAction<void, RootState, null, AnyAction> = () => {
     dispatch(fetchWeatherData(city));
@@ -30,9 +34,9 @@ const Weather: React.FC = () => {
     forecastData.forEach((forecast) => {
       const date = new Date(forecast.dt * 1000);
       const formattedDate = date.toLocaleDateString([], {
-        weekday: 'short', // 요일 표시
-        month: 'numeric', // 월 표시
-        day: 'numeric', //
+        weekday: "short", // 요일 표시
+        month: "numeric", // 월 표시
+        day: "numeric", //
       });
 
       if (!groupedForecastData[formattedDate]) {
@@ -58,7 +62,8 @@ const Weather: React.FC = () => {
         <div className="weather-container">
           <div>{weatherData.name}</div>
           <div>
-            기온 : {weatherData.main.temp.toFixed(1)}° / 습도 : {weatherData.main.humidity}%
+            기온 : {weatherData.main.temp.toFixed(1)}° / 습도 :{" "}
+            {weatherData.main.humidity}%
           </div>
           <div>날씨 : {weatherData.weather[0].main}</div>
         </div>
@@ -74,13 +79,26 @@ const Weather: React.FC = () => {
                   <div key={index} className="forecast-item">
                     <div>
                       {new Date(forecast.dt * 1000).toLocaleTimeString([], {
-                        hour: 'numeric',
+                        hour: "numeric",
                         hour12: true,
                       })}
                     </div>
                     <div>기온: {forecast.main.temp.toFixed(1)}°</div>
                     <div>습도: {forecast.main.humidity}%</div>
-                    <div>{forecast.weather[0].main}</div>
+                    <div>
+                      {forecast.weather[0].main === "Clouds" && (
+                        <img
+                          src={
+                            require("../weather-icon/free-icon-cloudy-5287586.png")
+                              .default
+                          }
+                          alt="Cloudy Icon"
+                          width="32"
+                          height="32"
+                        />
+                      )}
+                      <div>{forecast.weather[0].main}</div>
+                    </div>
                   </div>
                 ))}
               </div>
@@ -94,11 +112,10 @@ const Weather: React.FC = () => {
 
 export default Weather;
 
-
 // 아이디어 추가
 // 내일은 ~ 날씨가 이어져요
 // 내일 아침 최저온도는 오늘보다 ~도 낮아요
 // 일출.일몰.
-// 미세먼지 / 초미세 / 자외선지수 / 습도 / 바람 
+// 미세먼지 / 초미세 / 자외선지수 / 습도 / 바람
 // + 지금 일 + 4일까지만 데이터받아서 표현하기
 // + 주간예보
